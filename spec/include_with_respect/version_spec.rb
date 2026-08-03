3ca8df095bba04df98ec67c6805faa7be437fff7
Peter H. Boling <peter.boling@gmail.com>
2026-08-02 23:56:01 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/include_with_respect/version_spec.rb
+++ spec/include_with_respect/version_spec.rb
@@ -2,7 +2,6 @@
 
 require "anonymous_loader"
 RSpec.describe IncludeWithRespect::Version do
-
   it "executes the version file for coverage without redefining constants" do
     path = File.expand_path("../../lib/include_with_respect/version.rb", __dir__)
     anonymous_namespace = AnonymousLoader.load(files: path)
