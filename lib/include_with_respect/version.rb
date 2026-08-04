c19ba62bae01fe530ee61d42d648068b63baaada
Peter H. Boling <peter.boling@gmail.com>
2026-08-04 02:51:58 -0600

    Restore include with respect version namespace

--- lib/include_with_respect/version.rb
+++ lib/include_with_respect/version.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 module IncludeWithRespect
-  module Error
-    # Version namespace for this gem.
-    module Version
-      # Current gem version.
-      VERSION = "1.0.1"
-    end
-    # Current gem version exposed at the traditional constant location.
-    VERSION = Version::VERSION # Traditional Constant Location
+  # Version namespace for this gem.
+  module Version
+    # Current gem version.
+    VERSION = "1.0.1"
   end
 end
--- spec/include_with_respect/version_spec.rb
+++ spec/include_with_respect/version_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 require "anonymous_loader"
-RSpec.describe IncludeWithRespect::Error::Version do
+RSpec.describe IncludeWithRespect::Version do
   it "executes the version file for coverage without redefining constants" do
     path = File.expand_path("../../lib/include_with_respect/version.rb", __dir__)
     anonymous_namespace = AnonymousLoader.load(files: path)
 
-    expect(anonymous_namespace::IncludeWithRespect::Error::Version::VERSION).to eq(described_class::VERSION)
+    expect(anonymous_namespace::IncludeWithRespect::Version::VERSION).to eq(described_class::VERSION)
   end
 end
